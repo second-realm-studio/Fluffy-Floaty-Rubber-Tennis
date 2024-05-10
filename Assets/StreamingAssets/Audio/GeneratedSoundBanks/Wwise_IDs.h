ab4e7055843585458f103b1a8be41e666cadee63
sky <skyhaihai2000@gmail.com>
2024-05-10 13:04:39 -0600

    Setup wwise soundbank

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,56 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace STATES
+    {
+        namespace BGM
+        {
+            static const AkUniqueID GROUP = 412724365U;
+
+            namespace STATE
+            {
+                static const AkUniqueID COMBAT = 2764240573U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID SELECTION = 1973847113U;
+            } // namespace STATE
+        } // namespace BGM
+
+        namespace BGM_COMBAT
+        {
+            static const AkUniqueID GROUP = 736197432U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ENTER = 2368844905U;
+                static const AkUniqueID MAIN = 3161908922U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace BGM_COMBAT
+
+    } // namespace STATES
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
