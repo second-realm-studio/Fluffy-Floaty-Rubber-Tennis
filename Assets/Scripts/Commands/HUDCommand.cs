84bf2a7f19cc82ebe386b71ef5784a846e35bc63
sky <skyhaihai2000@gmail.com>
2024-05-10 01:07:55 -0600

    project setup for tojam2024

---
+++ Assets/Scripts/Commands/HUDCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using XiheFramework.Core;
+using XiheFramework.Runtime;
+
+namespace DevConsole.Commands {
+    public class HUDCommand : IDevConsoleCommand {
+        public bool Execute(string[] args) {
+            if (args.Length == 0) {
+                return false;
+            }
+
+            var arg0 = args[0].ToLower();
+            if (arg0 == "on") {
+                Game.UI.ActivateUI("HUD");
+                return true;
+            }
+            else if (arg0 == "off") {
+                Game.UI.UnactivateUI("HUD");
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Commands/PauseCommand.cs
@@ -0,0 +1,24 @@
+﻿using DevConsole;
+using XiheFramework.Runtime;
+
+namespace Commands {
+    public class PauseCommand : IDevConsoleCommand {
+        public bool Execute(string[] args) {
+            if (args.Length == 0 || args.Length > 1) {
+                return false;
+            }
+
+            if (args[0] == "on") {
+                Game.LogicTime.SetGlobalTimeScalePermanent(0f);
+            }
+            else if (args[0] == "off") {
+                Game.LogicTime.SetGlobalTimeScalePermanent(Game.LogicTime.defaultTimeScale);
+            }
+            else {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
