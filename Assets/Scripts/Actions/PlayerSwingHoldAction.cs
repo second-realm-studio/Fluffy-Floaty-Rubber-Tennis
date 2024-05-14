e4c9fce6a66740abba8b4f86e2476424bd4cce7f
sky <skyhaihai2000@gmail.com>
2024-05-13 20:40:53 -0600

    scene bake updated

--- Assets/Scripts/Actions/PlayerSwingHoldAction.cs
+++ Assets/Scripts/Actions/PlayerSwingHoldAction.cs
@@ -12,6 +12,7 @@ namespace Actions {
         public float chargeSpeed = 0.5f;
 
         private float m_SwingPower;
+        private Vector2 m_AimDir;
         private static readonly int SwingHolding = Animator.StringToHash("SwingHolding");
 
         protected override void OnActionInit() {
@@ -27,20 +28,23 @@ namespace Actions {
             //rotate player hand to face opposite direction of the left joystick input
             var aimDirH = Game.Input(owner.inputId).GetAxis(InputNames.AimHorizontal);
             var aimDirV = Game.Input(owner.inputId).GetAxis(InputNames.AimVertical);
-            var aimDir = new Vector2(aimDirH, aimDirV);
-            if (aimDir.magnitude > 0.1f) {
-                // owner.armRTransform.rotation = Quaternion.LookRotation(Vector3.back, -aimDir.ToVector3(V2ToV3Type.XY));
-                owner.armRTransform.rotation = Quaternion.Euler(0, 0, Vector3.SignedAngle(Vector3.up, -aimDir, Vector3.forward));
-            }
+            m_AimDir = new Vector2(aimDirH, aimDirV);
 
             if (Game.Input(owner.inputId).GetButtonDown(InputNames.SwingRelease)) {
                 ChangeAction(PlayerActionNames.PlayerSwingRelease,
-                    new KeyValuePair<string, object>(ActionArgumentNames.SwingDirection, aimDir.normalized),
+                    new KeyValuePair<string, object>(ActionArgumentNames.SwingDirection, m_AimDir.normalized),
                     new KeyValuePair<string, object>(ActionArgumentNames.SwingPower01, m_SwingPower));
             }
 
             if (!Game.Input(owner.inputId).GetButton(InputNames.SwingHold)) {
                 ChangeAction(PlayerActionNames.PlayerIdle);
+            }
+        }
+
+        private void LateUpdate() {
+            if (m_AimDir.magnitude > 0.1f) {
+                // owner.armRTransform.rotation = Quaternion.LookRotation(Vector3.back, -aimDir.ToVector3(V2ToV3Type.XY));
+                owner.armRTransform.rotation = Quaternion.Euler(0, 0, Vector3.SignedAngle(Vector3.up, -m_AimDir.ToVector3(V2ToV3Type.XY), Vector3.forward));
             }
         }
 
